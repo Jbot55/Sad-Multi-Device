37ddd88c2e90881ac13b44ba28729deba5accd15
MENGHAYYU <81160542+Jbot55@users.noreply.github.com>
2022-06-26 09:19:22 +0800

    Update config.js

--- config.js
+++ config.js
@@ -39,7 +39,7 @@ global.APIKeys = { // APIKey Here
   'https://leyscoders-api.herokuapp.com': 'dappakntlll',
   'https://violetics.pw': 'beta'
 }
-global.wm = '                「 ꜱᴀᴅ BOT あ⁩ 」' //Main Watermark
+global.wm = '                「 ꜱᴀᴅ BO あ⁩ 」' //Main Watermark
 
 global.wm2 = '꒷︶꒷꒥꒷ ‧₊˚ ꒰ฅ˘օառɛʀ - chαn˘ฅ ꒱ ‧₊˚꒷︶꒷꒥꒷'
 
